e7a03b12bca4e24eaf1e112a547d26861bf21427
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-05-24 15:50:43 -0400

    fix: remove iframe from index.html and create with js

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const iframe = document.getElementById('soundcloud-player');
+const iframe = document.getElementById("soundcloud-player");
 const player = SC.Widget(iframe);
-const img = document.querySelector('.img-content img:first-child');
+const img = document.querySelector(".img-content img:first-child");
 
-img.addEventListener('animationstart', () => {
-  player.play();
-});
+img.addEventListener("animationstart", () => {
+  const iframeTag = document.createElement("iframe");
 
-player.bind(SC.Widget.Events.FINISH, () => {
-  player.seekTo(0);
-  player.play();
+  iframeTag.id = "soundcloud-player";
+  iframeTag.width = "100%";
+  iframeTag.height = "166";
+  iframeTag.frameborder = "no";
+  iframeTag.allow = "autoplay";
+  iframeTag.src =
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1827478203&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false";
+  iframeTag.style.display = "none";
+  document.body.appendChild(iframeTag);
+  const player = SC.Widget(iframeTag);
+
+  player.bind(SC.Widget.Events.READY, () => {
+    player.play();
+  });
 });
